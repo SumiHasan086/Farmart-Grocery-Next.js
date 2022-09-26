7b8215b76b8691639859222727071bd2e7de4f2a
Sumi <sumihasan086@gmail.com>
2022-09-26 20:09:01 +0600

    Category done.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 
 import React from 'react'
+import HomeCategory from '../app/components/Home/HomeCategory'
 import HomeHero from '../app/components/Home/HomeHero'
 
 export default function Home (){
   return(
     <>
       <HomeHero/>
+      <HomeCategory />
     </>
   )
 }
